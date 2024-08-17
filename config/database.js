67c408313f8f6df5f6b5a5ab2375a77d2a4f7b61
rbiagini <rbiagini@gmail.com>
2024-08-17 09:48:06 -0300

    configurado var de ambiente

--- config/database.js
+++ config/database.js
@@ -1,10 +1,17 @@
 const { Sequelize } = require("sequelize");
 
+const d = process.env.DATABASE || "3001";
+
 // Substitua com as suas credenciais de banco de dados
-const sequelize = new Sequelize("fluxo_de_caixa", "root", "Adndko9873", {
-    host: "localhost",
-    dialect: "mysql", // ou 'postgres', 'sqlite', etc.
-});
+const sequelize = new Sequelize(
+    process.env.DATABASE || "fluxo_de_caixa",
+    process.env.DATABASE_USER || "root",
+    process.env.DATABASE_PASS || "Adndko9873",
+    {
+        host: process.env.DATABASE_HOST || "localhost",
+        dialect: "mysql", // ou 'postgres', 'sqlite', etc.
+    }
+);
 
 module.exports = sequelize;
 // module.exports = {Sequelize, sequelize} // o require deve virar uma variável para ser exportada
